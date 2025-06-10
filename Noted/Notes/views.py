a0dbf415e9037b01ca5e7b99a286e9b39a5a2abd
David Obasuyi <hondavidefosa@gmail.com>
2025-06-10 13:53:37 +0100

    updated templates, added authentication login and templated

--- Noted/Notes/views.py
+++ Noted/Notes/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Note
+from .forms import NoteForm
 
-# Create your views here.
+@login_required
+def note_list(request):
+    notes = Note.objects.filter(owner=request.user).order_by('-created_at')
+    return render(request, 'Notes/note_list.html', {'notes': notes})
+
+@login_required
+def note_detail(request, pk):
+    note = get_object_or_404(Note, pk=pk, owner=request.user)
+    return render(request, 'Notes/note_detail.html', {'note': note})
+
+@login_required
+def note_create(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            note = form.save(commit=False)
+            note.owner = request.user
+            note.save()
+            return redirect('note_list')
+    else:
+        form = NoteForm()
+    return render(request, 'Notes/note_form.html', {'form': form})
+
+@login_required
+def note_update(request, pk):
+    note = get_object_or_404(Note, pk=pk, owner=request.user)
+    if request.method == 'POST':
+        form = NoteForm(request.POST, instance=note)
+        if form.is_valid():
+            form.save()
+            return redirect('note_detail', pk=note.pk)
+    else:
+        form = NoteForm(instance=note)
+    return render(request, 'Notes/note_form.html', {'form': form})
+
+@login_required
+def note_delete(request, pk):
+    note = get_object_or_404(Note, pk=pk, owner=request.user)
+    if request.method == 'POST':
+        note.delete()
+        return redirect('note_list')
+    return render(request, 'Notes/note_confirm_delete.html', {'note': note})
