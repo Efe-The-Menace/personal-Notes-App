b152149f686bab1525f25d5cec9a85b1af5d32b5
David Obasuyi <hondavidefosa@gmail.com>
2025-06-23 20:05:43 +0100

    Commencement

--- Noted/Noted/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),  # Handles login/logout
-    path('', include('Notes.urls')),  # Your Notes app
-]
--- Noted/README.md
+++
@@ -1,2 +0,0 @@
-# personal-Notes-App
-A simple Django web app for creating, editing, and managing personal notes. Built to practice basic CRUD operations, Django models, views, templates, and forms.
