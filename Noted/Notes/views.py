f5ccf3a89441cea9138f5c49cdd3d9896be0ca62
David Obasuyi <hondavidefosa@gmail.com>
2025-06-26 12:48:01 +0100

    Completed delevopment. Awaiting styling

--- Noted/Notes/views.py
+++ Noted/Notes/views.py
@@ -3,12 +3,14 @@ from django.contrib.auth.decorators import login_required
 from .models import Note_data
 from .forms import note_form
 
+@login_required
 def list_view(request):
-     list = Note_data.objects.all()
+     list = Note_data.objects.filter(user=request.user)
      return render(request, 'Notes/note_list.html', {'list': list})
  
+@login_required
 def note_detail(request, pk):
-    note = get_object_or_404(Note_data, pk=pk)
+    note = get_object_or_404(Note_data, pk=pk, user=request.user)
     return render(request, 'Notes/note_detail.html', {'note': note})
 
 @login_required
@@ -16,10 +18,10 @@ def create_note(request):
     if request.method == 'POST':
         form = note_form(request.POST)
         if form.is_valid():
-            note = form.save(commit=False)  # Create, but don't save to DB yet
-            note.user = request.user        # Set the user manually
-            note.save()                     # Now save with user attached
-            return redirect('note_list')    # Update with your actual list view name
+            note = form.save(commit=False)
+            note.user = request.user
+            note.save()
+            return redirect('note_list')
     else:
         form = note_form()
 
@@ -37,10 +39,11 @@ def update_view(request, pk):
         form = note_form(instance=note)
     return render(request, 'Notes/form.html', {'form': form})
 
+
+@login_required
 def delete_note(request, pk):
     note = get_object_or_404(Note_data, pk=pk, user= request.user)
     if request.method == 'POST':
         note.delete()
         return redirect('note_list')
     return render(request, 'Notes/note_confirm_delete.html', {'note':note})
-    
--- Noted/user/views.py
+++ Noted/user/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import reg_form
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def register_user(request):
+    if request.method == 'POST':
+        form = reg_form(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success('Account created, You can proceed to Log In.')
+            return redirect('login')
+    else:
+        form = reg_form()
+    return render(request, 'user/register.html', {'form':form})
+    
+    
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data= request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, f'You are now logged in as {user}')
+            return redirect('note_list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'user/login.html', {'form': form})
+    
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out.")
+    return redirect('login')
+        
